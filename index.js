b1b06b1a348f519873bd48915b0fec254406c429
Maureenm41 <maureenmrchl@gmail.com>
2021-05-17 14:22:31 +0200

    up

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const mongoose = require("mongoose")
 
 require('dotenv').config()
+
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PW = process.env.MONGO_PW;
+const MONGO_DB = process.env.MONGO_DB;
