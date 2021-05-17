0871a634328c3173efeaa18bf1705daf0075267c
Maureenm41 <maureenmrchl@gmail.com>
2021-05-17 14:15:53 +0200

    up

--- connect.js
+++ connect.js
@@ -1,29 +1,42 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://root:<password>@test.f5w8t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
-
-
 const { MongoClient } = require("mongodb");
  
 // Replace the following with your Atlas connection string                                                                                                                                        
 const url = "mongodb+srv://<username>:<password>@clustername.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
 const client = new MongoClient(url);
-
-async function run() {
+ 
+ // The database to use
+ const dbName = "test";
+                      
+ async function run() {
     try {
-        await client.connect();
-        console.log("Connected correctly to server");
+         await client.connect();
+         console.log("Connected correctly to server");
+         const db = client.db(dbName);
 
-    } catch (err) {
-        console.log(err.stack);
-    }
-    finally {
+         // Use the collection "people"
+         const col = db.collection("people");
+
+         // Construct a document                                                                                                                                                              
+         let personDocument = {
+             "name": { "first": "Alan", "last": "Turing" },
+             "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
+             "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
+             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+             "views": 1250000
+         }
+
+         // Insert a single document, wait for promise so we can read it back
+         const p = await col.insertOne(personDocument);
+         // Find one document
+         const myDoc = await col.findOne();
+         // Print to the console
+         console.log(myDoc);
+
+        } catch (err) {
+         console.log(err.stack);
+     }
+ 
+     finally {
         await client.close();
     }
 }
