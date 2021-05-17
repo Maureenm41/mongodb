6a41073bef21587af3d3d9786c8f4c349d056b40
Maureenm41 <maureenmrchl@gmail.com>
2021-05-17 14:14:16 +0200

    up

--- connect.js
+++ connect.js
@@ -6,3 +6,26 @@ client.connect(err => {
   // perform actions on the collection object
   client.close();
 });
+
+
+
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://<username>:<password>@clustername.mongodb.net/test?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
